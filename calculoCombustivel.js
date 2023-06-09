d6ac6a952d095f0c2a5a357fcea9868a62176039
Ellielton <ellieltongama@gmail.com>
2023-06-09 07:14:11 -0300

    subindo a projeto barbearia2

---
+++ calculoCombustivel.js
@@ -0,0 +1,9 @@
+var tempo = 3
+var velocidade = 80
+var distancia = tempo * velocidade
+var litros = distancia/12
+
+console.log("A velocidade media foi", velocidade)
+console.log("O tempo gasto foi", tempo)
+console.log(distancia)
+console.log(combustivel)
